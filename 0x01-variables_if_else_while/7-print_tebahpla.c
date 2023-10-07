0b88b986f02e0ad3ba219dd40e4701f3bb3c27b2
simonebute <bedrockresources21@gmail.com>
2023-10-07 14:19:58 -0700

    task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - entry point
+ * return: always 0 (success)
+ */
+
+int main(void)
+
+{
+	char low;
+
+	for (low =  'z'; low >= 'a'; low--)
+	putchar(low);
+	putchar('\n');
+
+	return (0);
+
+}
