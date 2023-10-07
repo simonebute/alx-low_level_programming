0128e90894e766245262071c269f890442720153
simonebute <bedrockresources21@gmail.com>
2023-10-07 13:27:54 -0700

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+#include <stdlib.h>
+
+#include <time.h>
+
+/**
+ * main - entry point
+ * return: always 0 (success)
+ */
+
+int main(void)
+
+{
+	int i;
+
+	for (i = 0; i <= 10; i++)
+	printf("%d", i);
+	printf("\n");
+
+	return (0);
+
+}
