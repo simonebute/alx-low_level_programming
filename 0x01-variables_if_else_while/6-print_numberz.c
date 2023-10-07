9d3a06e88a7d469be67ca3588eebcd120df35b50
simonebute <bedrockresources21@gmail.com>
2023-10-07 13:44:36 -0700

    task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - entry point
+ * return: always 0 (success)
+ */
+
+int main(void)
+
+{
+	int d;
+
+	for (d = 0; d <= 9; d++)
+	putchar(d);
+	putchar('\n');
+
+	return (0);
+
+}
