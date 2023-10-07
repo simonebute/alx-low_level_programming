793556cb3f7dda11cecf8fcdf5eddd18b1c1fa14
simonebute <bedrockresources21@gmail.com>
2023-10-07 12:34:24 -0700

    task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabets
+ * return: always 0 (success)
+ */
+
+int main(void)
+
+{
+	char low;
+
+	for (low = 'a'; low <= 'z' ; low++)
+	putchar(low);
+
+	for (low = 'A'; low <= 'Z' ; low++)
+	putchar(low);
+
+	return (0);
+}
+
+
