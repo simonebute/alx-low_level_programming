2b94d77dde3b44793a46194d75960156e226a1e4
simonebute <bedrockresources21@gmail.com>
2023-10-17 09:13:13 -0700

    main headers

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,8 +1,15 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <stdio.h>
+#include <unistd.h>
+
 int _putchar(char c);
 void print_alphabet(void);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
+void _puts(char *str);
+void _putchar(char *str);
+void print_rev(char *s);
+
 #endif
