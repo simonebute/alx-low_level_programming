da601d6ee3e396943d2809b23cba1d450be9e3df
simonebute <bedrockresources21@gmail.com>
2023-10-17 12:03:02 -0700

    task 8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+* print_array - prints an array of integers
+* @a: array to be used
+* @n: number of elements to be printed
+* return: void
+*/
+
+void print_array(int *a, int n)
+
+{
+	int i;
+
+	for (i = 0 ; i < n ; i++)
+	{
+		printf("%d", a[i]);
+		if (i < n - 1)
+			printf(" , ");
+	printf("\n");
+	}
+
+}
