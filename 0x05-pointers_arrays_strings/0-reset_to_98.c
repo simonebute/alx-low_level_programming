e6a61cd5bd2607a9a57c6d433e869cc82c05276a
simonebute <bedrockresources21@gmail.com>
2023-10-16 17:27:40 -0700

    task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * resets_to_98 - resets n to 98
+ * @n: pointer to the value changed
+ * return: Always 0.
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
