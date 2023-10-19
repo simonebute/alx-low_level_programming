4f54af1cf1c2732a601600d90e31f3c32877d1b3
simonebute <bedrockresources21@gmail.com>
2023-10-19 10:28:30 -0700

    task 1

---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,16 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+
+int _putchar(char c);
+void print_alphabet(void);
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+int _strlen(char *s);
+void _puts(char *str);
+void print_rev(char *s);
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+#endif
