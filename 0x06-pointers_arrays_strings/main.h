29ff56c6adf1702c28234c0228a2760217563ee1
simonebute <bedrockresources21@gmail.com>
2023-10-19 11:11:23 -0700

    task 2

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -13,4 +13,5 @@ void _puts(char *str);
 void print_rev(char *s);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
 #endif
