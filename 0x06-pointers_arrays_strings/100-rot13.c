040550feb40a91744c1a15a0c07d598af19e5971
simonebute <bedrockresources21@gmail.com>
2023-10-20 12:20:22 -0700

    task 8

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+* rot13 - encoder rot13
+* @s: pointer to string parameters
+* Return: *s
+*/
+
+char *rot13(char *s)
+
+{
+	int i;
+	int j;
+	char c[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmopqrstuvwxyz";
+	char d[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; c[j] != '\0'; j++)
+		{
+			if (s[i] == c[j])
+			{
+				s[i] = d[j];
+				break;
+			}
+		}
+	}
+	return (s);
+}
