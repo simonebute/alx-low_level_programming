560263c89f9086792f3f7d4385cabcad65f751a7
simonebute <bedrockresources21@gmail.com>
2023-10-24 10:17:53 -0700

    task6

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+* print_chessboard - entry point
+* @: array
+* return: always 0 (success)
+*/
+
+void print_chessboard(char (*a)[8])
+
+{
+	int i, n;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (n = 0; n < 8; n++)
+			_putchar(a[i][n]);
+		_putchar('\n');
+	}
+
+}
