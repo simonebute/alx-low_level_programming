9de571bf44484d8544176c1de4194a472106c8f0
simonebute <bedrockresources21@gmail.com>
2023-10-26 11:56:37 -0700

    task 4

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* _pow_recursion - raises the power of x to y
+* @x: value to be raised
+* @y: power
+* Return: result of the power
+*/
+
+int _pow_recursion(int x, int y)
+
+{
+	if (y < 0)
+		return (-1);
+	if (y == 0)
+		return (1);
+	return (x * _pow_recursion(x, y - 1));
+
+}
