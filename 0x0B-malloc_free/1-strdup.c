78a77a905d74dba0dff540fbcfd863ee23a14de8
simonebute <bedrockresources21@gmail.com>
2023-10-30 12:34:51 -0700

    task1

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -9,14 +9,14 @@
 */
 
 char *_strdup(char *str)
-[
+{
 	int i;
 	char *copy;
 	int count = 0;
 
 	if (str == NULL)
 		return (NULL);
-	for (i == 0; str[i] != '\0'; i++)
+	for (i = 0; str[i] != '\0'; i++)
 		count++;
 
 	copy = malloc(sizeof(char) * count + 1);
