d49bd67e8eacec370acfb12a83379eb7edece089
simonebute <bedrockresources21@gmail.com>
2023-11-01 10:13:56 -0700

    main

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints all the main arguments
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return:0 success
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++);
+	{
+		printf("%s\n", argv[i]);
+	}
+
+	return (0);
+
+}
+
