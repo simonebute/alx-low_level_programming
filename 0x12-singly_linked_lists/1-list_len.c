c5b893c6b38edec3399314a400e742e741b03764
simonebute <bedrockresources21@gmail.com>
2023-11-16 11:00:13 -0800

    task1

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+* list_len - returns the number of elements in a linked list
+* @h: pointer to the list_t list
+*
+* Return: number of elements in h
+*/
+size_t list_len(const list_t *h)
+{
+	size_t elements = 0;
+	size_t n = 0;
+
+	while (h)
+	{
+		elements++;
+		n++;
+		h = h->next;
+	}
+
+	return (elements);
+	return (n);
+
+}
+
